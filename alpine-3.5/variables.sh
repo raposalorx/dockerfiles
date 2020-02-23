133361e398ea4d3c3acf52f24c88a191590bc217
Brian Robertson <brian@fulso.me>
2020-02-23 04:10:34 -0700

    Different alpine/openrc versions.
    PHP5 is only available in the main branch in 3.5

---
+++ alpine-3.5/variables.sh
@@ -0,0 +1,15 @@
+NAME=alpine
+VERSION=3.5
+PATCH_VERSION=3
+ARCH="x86_64"
+
+EXTRACT_CMD='tar -xzf'
+HASH_CMD='sha256sum'
+
+SRC='alpine-minirootfs-'$VERSION'.'$PATCH_VERSION'-'$ARCH
+SRC_FILE='alpine-minirootfs-'$VERSION'.'$PATCH_VERSION'-'$ARCH'.tar.gz'
+HASH_FILE=$SRC_FILE'.sha256'
+
+BASE_URL='http://dl-cdn.alpinelinux.org/alpine/v'$VERSION'/releases/'$ARCH'/'
+SRC_URL=$BASE_URL$SRC_FILE
+HASH_URL=$BASE_URL$HASH_FILE
---
+++ openrc-3.5/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source variables.sh
+
+# clean in reverse order
+chmod -x build.sh
+
+rm "Dockerfile"
